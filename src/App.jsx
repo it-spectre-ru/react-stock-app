e5cd417e34d4445fab40df7bd52704d863e3142d
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-02-14 14:04:20 +0000

    done routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { StockOverviewPage } from './pages/StockOverviewPage'
+import { StockDetailPage } from './pages/StockDetailPage'
 import './App.css'
 
 
@@ -8,7 +9,8 @@ export default function App() {
     <main>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<StockOverviewPage />}/>
+          <Route path="/" element={<StockOverviewPage />} />
+          <Route path="/detail/:symbol" element={<StockDetailPage />} />
         </Routes>
       </BrowserRouter>
     </main>

3a86e689c29a0fffd7fb4819fdf34eaa97a4fb6d
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-02-15 14:09:38 +0000

    get data from API

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,3 +1,21 @@
+import {useState, useEffect} from 'react'
+import finnHub from "../api/finnHub"
+
+
 export const StockList = () => {
+  const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN'])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get('/quote?symbol=MSFT&token=cflp5m1r01qjfr8f2u70cflp5m1r01qjfr8f2u7g')
+        console.log(response)
+      } catch (err) {
+        
+      }
+    }
+    fetchData()
+  }, [])
+  
   return <div>StockList</div>
 }

f6c08d88f254f1c1823d160fabe6abb59b2eb481
st ts <32254365+it-spectre-ru@users.noreply.github.com>
2023-02-28 05:40:43 +0000

    done

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,17 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { BsFillCaretDownFill, BsFillCaretUpFill } from "react-icons/bs"
 import finnHub from "../api/finnHub"
+import { WatchListContext } from "../context/watchListContext"
+
+
 
 
 export const StockList = () => {
-  const [stock, setStock] = useState()
-  const [watchList, setWatchList] = useState(['GOOGL', 'MSFT', 'AMZN'])
+  const [stock, setStock] = useState([])
+  const { watchList, deleteStock } = useContext(WatchListContext)
+  const navigate = useNavigate()
+
+
+  const changeColor = (change) => {
+    return change > 0 ? "success" : "danger"
+  }
+
+  const renderIcon = (change) => {
+    return change > 0 ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />
+  }
 
   useEffect(() => {
     let isMounted = true
     const fetchData = async () => {
+
       try {
         const responses = await Promise.all(watchList.map((stock) => {
-          return finnHub.get('/quote', {
+          return finnHub.get("/quote", {
             params: {
               symbol: stock
             }
@@ -24,6 +40,7 @@ export const StockList = () => {
             data: response.data,
             symbol: response.config.params.symbol
           }
+
         })
         console.log(data)
         if (isMounted) {
@@ -37,34 +54,43 @@ export const StockList = () => {
     fetchData()
 
     return () => (isMounted = false)
-  }, [])
+  }, [watchList])
+
+  const handleStockSelect = (symbol) => {
+    navigate(`detail/${symbol}`)
+  }
+
 
   return (<div>
     <table className="table hover mt-5">
-      <thead style={{color: "rgb(79,89,102)"}}>
+      <thead style={{ color: "rgb(79,89,102)" }}>
         <tr>
           <th scope="col">Name</th>
           <th scope="col">Last</th>
           <th scope="col">Chg</th>
           <th scope="col">Chg%</th>
-          <th scope="col">Hight</th>
+          <th scope="col">High</th>
           <th scope="col">Low</th>
           <th scope="col">Open</th>
           <th scope="col">Pclose</th>
+
         </tr>
       </thead>
       <tbody>
         {stock.map((stockData) => {
           return (
-            <tr className="table-row" key={stockData.symbol}>
+            <tr style={{ cursor: "pointer" }} onClick={() => handleStockSelect(stockData.symbol)} className="table-row" key={stockData.symbol}>
               <th scope="row">{stockData.symbol}</th>
               <td>{stockData.data.c}</td>
-              <td>{stockData.data.d}</td>
-              <td>{stockData.data.dp}</td>
+              <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d} {renderIcon(stockData.data.d)}</td>
+              <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.dp} {renderIcon(stockData.data.d)} </td>
               <td>{stockData.data.h}</td>
               <td>{stockData.data.l}</td>
               <td>{stockData.data.o}</td>
-              <td>{stockData.data.pc}</td>
+              <td>{stockData.data.pc} <button className="btn btn-danger btn-sm ml-3 d-inline-block delete-button" onClick={(e) => {
+                e.stopPropagation()
+                deleteStock(stockData.symbol)
+              }}>Remove</button></td>
             </tr>
           )
         })}
--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,10 +1,11 @@
-import {AutoComplete} from "../components/AutoComplete"
-import {StockList} from "../components/StockList"
-
-
-
+import { AutoComplete } from "../components/AutoComplete"
+import { StockList } from "../components/StockList"
+import trading from "../images/Trading.png"
 export const StockOverviewPage = () => {
-  return <div>StockOverviewPage
+  return <div>
+    <div className="text-center">
+      <img src={trading} />
+    </div>
     <AutoComplete />
     <StockList />
   </div>
